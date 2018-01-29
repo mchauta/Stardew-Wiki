f414a20a94748e48fa79941fbf5250dd191154b8
Matt Chauta <mchauta@nrao.edu>
2018-01-29 12:01:23 -0500

    adding new screens for links

--- src/components/SingleCat.js
+++ src/components/SingleCat.js
@@ -1,8 +1,69 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, WebView } from "react-native";
+import { StyleSheet, Text, View, WebView, ScrollView } from "react-native";
 import PropTypes from 'prop-types';
 import { StackNavigator } from 'react-navigation';
 import ajax from '../ajax';
+import HTMLView from 'react-native-htmlview';
+
+
+class SingleCat extends React.Component {
+
+
+
+  static propTypes = {
+    //category: PropTypes.object.isRequired,
+  };
+
+  state = {
+    data: [],
+  };
+  async componentDidMount() {
+     const { params } = this.props.navigation.state;
+     const singlePageData = await ajax.fetchSinglePage (params.pageName);
+     this.setState({ data: singlePageData });
+     console.log(this.state.data);
+     console.log(this.state.data.parse.text['*']);
+
+  }
+
+  static navigationOptions = ({ navigation }) => ({
+
+    title: navigation.state.params.pageName,
+  })
+
+
+
+  render() {
+    const { params } = this.props.navigation.state;
+    const { navigate } = this.props.navigation;
+    return (
+
+      <View style={styles.container}>
+
+        {this.state.data.parse ? (
+          <ScrollView>
+              <HTMLView
+                style={styles.web}
+                value={this.state.data.parse.text['*']}
+                stylesheet={webstyles}
+                onLinkPress={(url) => navigate("SingleCat", {pageName: url.replace("/","")})}
+
+              />
+          </ScrollView>
+
+       ) : (
+         <Text style={styles.welcome}>Loading...</Text>
+       )}
+
+      </View>
+
+    );
+  }
+}
+
+var webstyles = StyleSheet.create({
+
+})
 
 const styles = StyleSheet.create({
   container: {
@@ -19,58 +80,8 @@ const styles = StyleSheet.create({
   web: {
     width: '100%',
     flex: 1,
+    overflow: 'scroll',
   }
 });
-
-class SingleCat extends React.Component {
-
-  static propTypes = {
-    //category: PropTypes.object.isRequired,
-  };
-
-  state = {
-    data: [],
-  };
-  async componentDidMount() {
-     const { params } = this.props.navigation.state;
-     const singlePageData = await ajax.fetchSinglePage (params.pageName);
-     this.setState({ data: singlePageData });
-     console.log(this.state.data);
-
-
-  }
-
-  static navigationOptions = ({ navigation }) => ({
-
-    title: navigation.state.params.pageName,
-  })
-
-
-
-  render() {
-    const { params } = this.props.navigation.state;
-    return (
-
-      <View style={styles.container}>
-
-        {this.state.data.parse ? (
-
-          <WebView
-            style={styles.web}
-            source={{uri: 'https://stardewvalleywiki.com/?curid=' + this.state.data.parse.pageid}}
-            javascriptEnable= {false}
-            thirdPartyCookiesEnabled={false}
-          />
-
-       ) : (
-         <Text style={styles.welcome}>Loading...</Text>
-       )}
-
-      </View>
-    );
-  }
-}
-
-
 
 export default SingleCat;
